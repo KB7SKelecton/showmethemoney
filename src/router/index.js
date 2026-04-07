2fa30b999c21b6b1085aa108443d0b28b4778ffb
nanemo4168-arch <nanemo4168@gmail.com>
2026-04-07 16:25:36 +0900

    Remove About and Report pages; add MyPage

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '@/pages/Home.vue';
-import About from '@/pages/About.vue';
 import List from '@/pages/List.vue';
-import Report from '@/pages/Report.vue';
+import MyPage from '@/pages/MyPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: Home },
-    { path: '/about', name: 'about', component: About },
     { path: '/list', name: 'list', component: List },
-    { path: '/report', name: 'report', component: Report },
+    { path: '/mypage', name: 'mypage', component: MyPage },
   ],
 });
 
